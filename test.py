a4c4c5d615cbbf0bbc00d23ae3cc65f69eea54fc
Vladimir Khvorostov <vladimir@khvorostov.ru>
2023-10-18 14:13:45 +0700

    kuznyechik for text strings

--- test.py
+++ test.py
@@ -1,17 +1,39 @@
 from cipher.gost2015 import gost2015
-import binascii
 
-mtest = list(binascii.unhexlify('1122334455667700ffeeddccbbaa9988'))
-ktest = list(binascii.unhexlify('8899aabbccddeeff0011223344556677fedcba98765432100123456789abcdef'))
-print(binascii.unhexlify('8899aabbccddeeff0011223344556677fedcba98765432100123456789abcdef'))
+def kuznyechik_encrypt(gost, plaintext: bytes):
+    ciphertext = bytearray()
+    
+    for i in range(0, len(plaintext), 16):
+        block = plaintext[i:i+16].ljust(16, b'\0')  # Pad the last block if needed
+        bytes_block = list(block)
+        encrypted_block = gost.encryption(bytes_block)
+        ciphertext.extend(encrypted_block)
+    
+    return bytes(ciphertext)
+
+def kuznyechik_decrypt(gost, ciphertext: bytes):
+    plaintext = bytearray()
+    
+    for i in range(0, len(ciphertext), 16):
+        block = ciphertext[i:i+16]
+        bytes_block = bytes_block = list(block)
+        decrypted_block = gost.decryption(bytes_block)
+        plaintext.extend(decrypted_block)
+    
+    return bytes(plaintext)
+
+
+mtest = '{"data": "Hello world"}'.encode('utf-8')
+ktest = list(bytes.fromhex('8899aabbccddeeff0011223344556677fedcba98765432100123456789abcdef'))
 gost = gost2015(ktest)
-c = gost.encryption(mtest)
-res = binascii.hexlify(bytearray(c))
+c = kuznyechik_encrypt(gost, mtest)
+res = bytearray(c).hex()
 
+print(mtest)
 print(res)
 
-dtest = list(binascii.unhexlify(res))
-d = gost.decryption(dtest)
-dres = binascii.hexlify(bytearray(d))
+dtest = bytes.fromhex(res)
+d = kuznyechik_decrypt(gost, dtest)
+dres = bytearray(d).decode('utf-8')
 
 print(dres)
